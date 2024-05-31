090836c60bc07c23bc9950edb963f464238f6e98
Nathan Brader <nbrader@gmail.com>
2024-05-31 14:22:31 +0100

    flattened the tree of asserts

--- Coq/CubeDiffVol/CubeDiffVol.v
+++ Coq/CubeDiffVol/CubeDiffVol.v
@@ -92,59 +92,56 @@ Proof.
     h^3 * (1 - (2/3)^3) = cube_vol (h * 19^(1/3)/3)
   *)
 
-  assert (1 - Rpower (2 / 3) 3 = 19/(Rpower 3 3)).
-  - rewrite (Rmult_inv 2 3) by lra.
-    assert (0 < 2) by lra.
-    assert (0 < /3) by lra.
-    rewrite <- (Rpower_mult_distr 2 (/3) 3 H0 H1). clear H0 H1.
-    rewrite (Rpower_Ropp_corollary_2 3 3) by lra.
-    assert (Rpower 3 3 <> 0) by (rewrite Rpower_3_3; lra).
-    rewrite <- (Rinv_r (Rpower 3 3) H0).
-    rewrite <- (Rmult_minus_distr_r (/ Rpower 3 3) (Rpower 3 3) (Rpower 2 3)).
-    rewrite Rmult_inv by lra.
-    f_equal.
-    rewrite Rpower_2_3.
-    rewrite Rpower_3_3.
-    lra.
-  - rewrite H0. clear H0.
+  assert (1 - Rpower (2 / 3) 3 = 19/(Rpower 3 3)) by (
+    rewrite (Rmult_inv 2 3) by lra;
+    assert (0 < 2) by lra;
+    assert (0 < /3) by lra;
+    rewrite <- (Rpower_mult_distr 2 (/3) 3 H0 H1); clear H0 H1;
+    rewrite (Rpower_Ropp_corollary_2 3 3) by lra;
+    assert (Rpower 3 3 <> 0) by (rewrite Rpower_3_3; lra);
+    rewrite <- (Rinv_r (Rpower 3 3) H0);
+    rewrite <- (Rmult_minus_distr_r (/ Rpower 3 3) (Rpower 3 3) (Rpower 2 3));
+    rewrite Rmult_inv by lra;
+    f_equal;
+    rewrite Rpower_2_3;
+    rewrite Rpower_3_3;
+    lra
+  ).
+  rewrite H0. clear H0.
   (*
     h^3 * 19/3^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-    assert (0 < 19) by lra.
-    assert (/3 <> 0) by lra.
-    rewrite <- (Rpower_recip_inv 19 (/3) H0 H1) at 1. clear H0 H1.
-    replace (1 / / 3) with 3 by lra.
+  assert (0 < 19) by lra.
+  assert (/3 <> 0) by lra.
+  rewrite <- (Rpower_recip_inv 19 (/3) H0 H1) at 1. clear H0 H1.
+  replace (1 / / 3) with 3 by lra.
   (*
     h^3 * 19^(1/3)^3/3^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-    assert (Rpower 3 3 <> 0).
-    + rewrite Rpower_3_3.
-      lra.
-    + rewrite (Rmult_inv (Rpower (Rpower 19 (/ 3)) 3) (Rpower 3 3) H0). clear H0.
-      rewrite <- Rpower_Ropp_corollary_2 by lra.
-      assert (0 < Rpower 19 (/ 3)).
-      * unfold Rpower.
-        apply exp_pos.
-      * assert (0 < /3) by lra.
-        rewrite (Rpower_mult_distr ((Rpower 19 (/ 3))) (/ 3) 3 H0 H1). clear H1.
+  assert (Rpower 3 3 <> 0) by (rewrite Rpower_3_3; lra).
+  rewrite (Rmult_inv (Rpower (Rpower 19 (/ 3)) 3) (Rpower 3 3) H0). clear H0.
+  rewrite <- Rpower_Ropp_corollary_2 by lra.
+  assert (0 < Rpower 19 (/ 3)) by (unfold Rpower; apply exp_pos).
+  assert (0 < /3) by lra.
+  rewrite (Rpower_mult_distr ((Rpower 19 (/ 3))) (/ 3) 3 H0 H1). clear H1.
   (*
     h^3 * (19^(1/3)*(1/3))^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-        assert (0 < Rpower 19 (/ 3) * / 3) by lra. clear H0.
-        rewrite (Rpower_mult_distr h (Rpower 19 (/ 3) * / 3) 3 H H1). clear H H1.
-        rewrite <- Rmult_assoc.
-        rewrite <- Rmult_inv by lra.
+  assert (0 < Rpower 19 (/ 3) * / 3) by lra. clear H0.
+  rewrite (Rpower_mult_distr h (Rpower 19 (/ 3) * / 3) 3 H H1). clear H H1.
+  rewrite <- Rmult_assoc.
+  rewrite <- Rmult_inv by lra.
   (*
     (h*19^(1/3)/3)^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-        fold (cube_vol (h * Rpower 19 (/ 3) / 3)).
+  fold (cube_vol (h * Rpower 19 (/ 3) / 3)).
   (*
     cube_vol (h*19^(1/3)/3) = cube_vol (h * 19^(1/3)/3)
   *)
 
-        reflexivity.
+  reflexivity.
 Qed.
