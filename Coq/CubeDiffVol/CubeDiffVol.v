9b6d34816bd5a8d0d8bd8d8b7fb739cedbf3e105
Nathan Brader <nbrader@gmail.com>
2024-05-31 15:00:36 +0100

    Simplified proof.

--- Coq/CubeDiffVol/CubeDiffVol.v
+++ Coq/CubeDiffVol/CubeDiffVol.v
@@ -84,22 +84,19 @@ Proof.
     h^3 - (h*2/3)^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-  assert (0 < 2/3) by lra.
-  rewrite <- (Rpower_mult_distr h (2 / 3) 3 H H0). clear H0.
+  rewrite <- Rpower_mult_distr with (x := h) (y := 2/3) (z := 3); try lra.
   rewrite <- (Rmult_1_r (Rpower h 3)) at 1.
   rewrite <- Rmult_minus_distr_l.
   (*
     h^3 * (1 - (2/3)^3) = cube_vol (h * 19^(1/3)/3)
   *)
 
-  assert (1 - Rpower (2 / 3) 3 = 19/(Rpower 3 3)) by (
+  replace (1 - Rpower (2 / 3) 3) with (19/(Rpower 3 3)) by (
     rewrite (Rmult_inv 2 3) by lra;
-    assert (0 < 2) by lra;
-    assert (0 < /3) by lra;
-    rewrite <- (Rpower_mult_distr 2 (/3) 3 H0 H1); clear H0 H1;
+    rewrite <- (Rpower_mult_distr 2 (/3) 3) by lra;
     rewrite (Rpower_Ropp_corollary_2 3 3) by lra;
     assert (Rpower 3 3 <> 0) by (rewrite Rpower_3_3; lra);
-    rewrite <- (Rinv_r (Rpower 3 3) H0);
+    rewrite <- (Rinv_r (Rpower 3 3)) by lra;
     rewrite <- (Rmult_minus_distr_r (/ Rpower 3 3) (Rpower 3 3) (Rpower 2 3));
     rewrite Rmult_inv by lra;
     f_equal;
@@ -107,27 +104,23 @@ Proof.
     rewrite Rpower_3_3;
     lra
   ).
-  rewrite H0. clear H0.
   (*
     h^3 * 19/3^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
-  assert (0 < 19) by lra.
-  assert (/3 <> 0) by lra.
-  rewrite <- (Rpower_recip_inv 19 (/3) H0 H1) at 1. clear H0 H1.
+  rewrite <- (Rpower_recip_inv 19 (/3)) at 1 by lra.
   replace (1 / / 3) with 3 by lra.
   (*
     h^3 * 19^(1/3)^3/3^3 = cube_vol (h * 19^(1/3)/3)
   *)
 
   assert (Rpower 3 3 <> 0) by (rewrite Rpower_3_3; lra).
-  rewrite (Rmult_inv (Rpower (Rpower 19 (/ 3)) 3) (Rpower 3 3) H0). clear H0.
+  rewrite (Rmult_inv (Rpower (Rpower 19 (/ 3)) 3) (Rpower 3 3)) by lra.
   rewrite <- Rpower_Ropp_corollary_2 by lra.
   assert (0 < Rpower 19 (/ 3)) by (unfold Rpower; apply exp_pos).
-  assert (0 < /3) by lra.
-  rewrite (Rpower_mult_distr ((Rpower 19 (/ 3))) (/ 3) 3 H0 H1). clear H1.
-  assert (0 < Rpower 19 (/ 3) * / 3) by lra. clear H0.
-  rewrite (Rpower_mult_distr h (Rpower 19 (/ 3) * / 3) 3 H H1). clear H1.
+  rewrite (Rpower_mult_distr ((Rpower 19 (/ 3))) (/ 3) 3) by lra.
+  rewrite (Rpower_mult_distr h (Rpower 19 (/ 3) * / 3) 3) by lra.
+  clear H0 H1.
   rewrite <- Rmult_assoc.
   rewrite <- Rmult_inv by lra.
   (*
